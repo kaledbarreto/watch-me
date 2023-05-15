1475552719b1b9d8f01b2faf35041e7ab231fd75
Kaled Barreto <kaledbarreto@gmail.com>
2023-05-15 19:17:00 -0300

    feat: add show more

---
+++ src/pages/home/detailed/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var mySpan = document.getElementById("show-more");
+    mySpan.addEventListener("click", function() {
+      window.location.href="http://000.0.0.0:5500/src/pages/home/";
+    });
+  });
