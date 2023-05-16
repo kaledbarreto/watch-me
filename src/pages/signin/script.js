5409360425ca71b6c1db22570ac97256edd30276
Arthur Passos <arthur.passos@cubos.io>
2023-05-16 20:35:10 +0000

    fix: change IP to localhost

--- src/pages/signin/script.js
+++ src/pages/signin/script.js
@@ -1,12 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("signup-form").addEventListener("submit", function(event) {
-    window.location.href="http://000.0.0.0:5500/src/pages/signup/";
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("signup-form")
+    .addEventListener("submit", function (event) {
+      window.location.href = "http://127.0.0.1:5500/src/pages/signup/";
+    });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var mySpan = document.getElementById("signup");
-  mySpan.addEventListener("click", function() {
-    window.location.href="http://000.0.0.0:5500/src/pages/signup/";
+  mySpan.addEventListener("click", function () {
+    window.location.href = "http://127.0.0.1:5500/src/pages/signup/";
   });
 });
--- src/pages/signup/script.js
+++ src/pages/signup/script.js
@@ -1,12 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("signup-form").addEventListener("submit", function(event) {
-    window.location.href="http://000.0.0.0:5500/src/pages/signin/";
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("signup-form")
+    .addEventListener("submit", function (event) {
+      window.location.href = "http://127.0.0.1:5500/src/pages/signin/";
+    });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var mySpan = document.getElementById("signin");
-  mySpan.addEventListener("click", function() {
-    window.location.href="http://000.0.0.0:5500/src/pages/signin/";
+  mySpan.addEventListener("click", function () {
+    window.location.href = "http://127.0.0.1:5500/src/pages/signin/";
   });
 });
